1d6c8fe5496aec33c18d8a38165d5acc191aa334
mahmoudhamdyae <mahmoudhamdyae@gmail.com>
2024-02-14 11:00:31 +0200

    Course Screen

--- lib/presentation/screens/courses/screens/course_screen.dart
+++ lib/presentation/screens/courses/screens/course_screen.dart
@@ -20,109 +20,90 @@ class CourseScreen extends StatelessWidget {
     return Scaffold(
       body: Stack(
         children: [
-          Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Image.asset(
-                ImageAssets.course,
-                height: 240,
-              ),
-              // Subject Name
-              Padding(
-                padding: const EdgeInsets.all(16.0),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    Text(
-                      course.name ?? '',
-                      style: getLargeStyle(),
-                    ),
-                    // 5 وحدات - 32 درس
-                    Padding(
-                      padding: const EdgeInsets.symmetric(horizontal: 16.0),
-                      child: Text(
-                        '5 وحدات - 32 درس',
-                        style: getSmallStyle(),
-                      ),
-                    ),
-                  ],
+          SizedBox(
+            height: double.infinity,
+            child: ListView(
+              shrinkWrap: true,
+              physics: const ClampingScrollPhysics(),
+              children: [
+                Image.asset(
+                  ImageAssets.course,
+                  height: 240,
                 ),
-              ),
-              Stack(
-                children: [
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceAround,
+                // Subject Name
+                Padding(
+                  padding: const EdgeInsets.all(16.0),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
                     children: [
-                      PriceWidget(price: course.month ?? 0, month: AppStrings.monthly,),
-                      PriceWidget(price: course.term ?? 0, month: AppStrings.termly,),
+                      Text(
+                        course.name ?? '',
+                        style: getLargeStyle(),
+                      ),
+                      // 5 وحدات - 32 درس
+                      Padding(
+                        padding: const EdgeInsets.symmetric(horizontal: 16.0),
+                        child: Text(
+                          '5 وحدات - 32 درس',
+                          style: getSmallStyle(),
+                        ),
+                      ),
                     ],
                   ),
-                  const Positioned(
-                      right: 0,
-                      left: 0,
-                      top: 0,
-                      bottom: 0,
-                      child: VerticalDivider(width: 1, color: ColorManager.lightGrey,)
-                  )
-                ],
-              ),
-              const Padding(
-                padding: EdgeInsets.symmetric(horizontal: 16.0, vertical: 16.0),
-                child: Divider(height: 1, color: Color(0xffF2F2F2),),
-              ),
-              // المدرس
-              Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 16.0),
-                child: Text(
-                  AppStrings.courseTeacher,
-                  style: getLargeStyle(),
                 ),
-              ),
-              Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 16.0, vertical: 16.0),
-                child: Row(
+                Stack(
                   children: [
-                    Image.asset(
-                      ImageAssets.user,
-                      height: 40,
-                      width: 40,
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceAround,
+                      children: [
+                        PriceWidget(price: course.month ?? 0, month: AppStrings.monthly,),
+                        PriceWidget(price: course.term ?? 0, month: AppStrings.termly,),
+                      ],
                     ),
-                    // Teacher Name
-                    Padding(
-                      padding: const EdgeInsets.symmetric(horizontal: 8.0),
-                      child: Text(
-                        course.teacher ?? '',
-                        style: getSmallStyle(),
-                      ),
-                    ),
+                    const Positioned(
+                        right: 0,
+                        left: 0,
+                        top: 0,
+                        bottom: 0,
+                        child: VerticalDivider(width: 1, color: ColorManager.lightGrey,)
+                    )
                   ],
                 ),
-              ),
-              Expanded(child: Container()),
-              // شاهد الدرس الأول
-              Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 16.0),
-                child: SizedBox(
-                  width: double.infinity,
-                  child: OutlinedButton(
-                    style: getOutlinedButtonStyle(),
-                    onPressed: () {
-                      CoursesController controller = Get.find<CoursesController>();
-                      controller.course.value = course;
-                      controller.getTutorials();
-                      Get.to(() => const LessonScreen());
-                    },
-                    child: Text(
-                      AppStrings.watchFirstLesson,
-                      style: getSmallStyle(
-                        color: ColorManager.primary,
-                      ),
-                    ),
+                const Padding(
+                  padding: EdgeInsets.symmetric(horizontal: 16.0, vertical: 16.0),
+                  child: Divider(height: 1, color: Color(0xffF2F2F2),),
+                ),
+                // المدرس
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 16.0),
+                  child: Text(
+                    AppStrings.courseTeacher,
+                    style: getLargeStyle(),
                   ),
                 ),
-              ),
-              const SizedBox(height: 8.0,),
-            ],
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 16.0, vertical: 16.0),
+                  child: Row(
+                    children: [
+                      Image.asset(
+                        ImageAssets.user,
+                        height: 40,
+                        width: 40,
+                      ),
+                      // Teacher Name
+                      Padding(
+                        padding: const EdgeInsets.symmetric(horizontal: 8.0),
+                        child: Text(
+                          course.teacher ?? '',
+                          style: getSmallStyle(),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                const SizedBox(height: 64.0,),
+              ],
+            ),
           ),
           // Back Button
           IconButton(
@@ -136,6 +117,30 @@ class CourseScreen extends StatelessWidget {
                   color: ColorManager.black,
                 ),
               )
+          ),
+          // شاهد الدروس
+          Positioned(
+            bottom: 8.0,
+            right: 16.0,
+            left: 16.0,
+            child: SizedBox(
+              width: double.infinity,
+              child: FilledButton(
+                style: getFilledButtonStyle(),
+                onPressed: () {
+                  CoursesController controller = Get.find<CoursesController>();
+                  controller.course.value = course;
+                  controller.getTutorials();
+                  Get.to(() => const LessonScreen());
+                },
+                child: Text(
+                  AppStrings.watchFirstLesson,
+                  style: getSmallStyle(
+                    color: ColorManager.white,
+                  ),
+                ),
+              ),
+            ),
           ),
         ],
       ),
