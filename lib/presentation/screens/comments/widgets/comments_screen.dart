f63ea47de0ea5e54e5ffe9c70d183c6425481a19
mahmoudhamdyae <mahmoudhamdyae@gmail.com>
2024-02-19 12:10:53 +0200

    Notifications List

--- lib/presentation/screens/comments/widgets/comments_screen.dart
+++ lib/presentation/screens/comments/widgets/comments_screen.dart
@@ -5,40 +5,39 @@ import '../../../widgets/home_app_bar/home_app_bar.dart';
 import '../../../widgets/notification_item.dart';
 
 class CommentsScreen extends StatelessWidget {
-  final List<NotificationModel> notifications = [
-    NotificationModel(
-        'هناك حقيقة مثبتة منذ زمن طويل وهي أن المحتوى المقروء لصفحة ما سيلهي القارئ عن التركيز على الشكل الخارجي للنص',
-        '3 ساعة'),
-    NotificationModel(
-        'هناك حقيقة مثبتة منذ زمن طويل وهي أن المحتوى المقروء لصفحة ما سيلهي القارئ عن التركيز على الشكل الخارجي للنص',
-        '12 سبتمبر'),
-    NotificationModel(
-        'هناك حقيقة مثبتة منذ زمن طويل وهي أن المحتوى المقروء لصفحة ما سيلهي القارئ عن التركيز على الشكل الخارجي للنص',
-        '12 سبتمبر'),
-  ];
+  final List<NotificationModel> notifications = List.generate(20, (index) => NotificationModel(
+      'هناك حقيقة مثبتة منذ زمن طويل وهي أن المحتوى المقروء لصفحة ما سيلهي القارئ عن التركيز على الشكل الخارجي للنص',
+      '3 ساعة')
+  );
 
   CommentsScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: ListView(
-        shrinkWrap: true,
-        physics: const ClampingScrollPhysics(),
-        children: [
-          HomeAppBar(),
-          ListView.builder(
-            shrinkWrap: true,
-            physics: const ClampingScrollPhysics(),
-            itemCount: notifications.length,
-            itemBuilder: (BuildContext context, int index) {
-              return NotificationItem(
-                notification: notifications[index],
-              );
-            },
-          ),
-        ],
+      body: RefreshIndicator(
+        onRefresh: () async => await _getComments(),
+        child: ListView(
+          shrinkWrap: true,
+          physics: const AlwaysScrollableScrollPhysics(),
+          children: [
+            HomeAppBar(),
+            ListView.builder(
+              shrinkWrap: true,
+              physics: const ClampingScrollPhysics(),
+              itemCount: notifications.length,
+              itemBuilder: (BuildContext context, int index) {
+                return NotificationItem(
+                  notification: notifications[index],
+                );
+              },
+            ),
+          ],
+        ),
       ),
     );
+  }
+
+  Future<void> _getComments() async {
   }
 }
